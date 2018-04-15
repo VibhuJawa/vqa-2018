8c4c5c1e9fc1daf6041a8392145210e1daf13f19
Praateek Mahajan <praateekm@gmail.com>
2018-04-15 19:34:09 -0400

    Python2 to 3

--- data/PythonHelperTools/vqaDemo.py
+++ data/PythonHelperTools/vqaDemo.py
@@ -6,33 +6,33 @@ import skimage.io as io
 import matplotlib.pyplot as plt
 import os
 
-dataDir		='../../VQA'
-versionType ='v2_' # this should be '' when using VQA v2.0 dataset
-taskType    ='OpenEnded' # 'OpenEnded' only for v2.0. 'OpenEnded' or 'MultipleChoice' for v1.0
-dataType    ='mscoco'  # 'mscoco' only for v1.0. 'mscoco' for real and 'abstract_v002' for abstract for v1.0.
-dataSubType ='train2014'
-annFile     ='%s/Annotations/%s%s_%s_annotations.json'%(dataDir, versionType, dataType, dataSubType)
-quesFile    ='%s/Questions/%s%s_%s_%s_questions.json'%(dataDir, versionType, taskType, dataType, dataSubType)
-imgDir 		= '%s/Images/%s/%s/' %(dataDir, dataType, dataSubType)
+dataDir = '../../VQA'
+versionType = 'v2_'  # this should be '' when using VQA v2.0 dataset
+taskType = 'OpenEnded'  # 'OpenEnded' only for v2.0. 'OpenEnded' or 'MultipleChoice' for v1.0
+dataType = 'mscoco'  # 'mscoco' only for v1.0. 'mscoco' for real and 'abstract_v002' for abstract for v1.0.
+dataSubType = 'train2014'
+annFile = '%s/Annotations/%s%s_%s_annotations.json' % (dataDir, versionType, dataType, dataSubType)
+quesFile = '%s/Questions/%s%s_%s_%s_questions.json' % (dataDir, versionType, taskType, dataType, dataSubType)
+imgDir = '%s/Images/%s/%s/' % (dataDir, dataType, dataSubType)
 
 # initialize VQA api for QA annotations
-vqa=VQA(annFile, quesFile)
+vqa = VQA(annFile, quesFile)
 
 # load and display QA annotations for given question types
 """
 All possible quesTypes for abstract and mscoco has been provided in respective text files in ../QuestionTypes/ folder.
 """
-annIds = vqa.getQuesIds(quesTypes='how many');   
+annIds = vqa.getQuesIds(quesTypes='how many');
 anns = vqa.loadQA(annIds)
 randomAnn = random.choice(anns)
 vqa.showQA([randomAnn])
 imgId = randomAnn['image_id']
-imgFilename = 'COCO_' + dataSubType + '_'+ str(imgId).zfill(12) + '.jpg'
+imgFilename = 'COCO_' + dataSubType + '_' + str(imgId).zfill(12) + '.jpg'
 if os.path.isfile(imgDir + imgFilename):
-	I = io.imread(imgDir + imgFilename)
-	plt.imshow(I)
-	plt.axis('off')
-	plt.show()
+    I = io.imread(imgDir + imgFilename)
+    plt.imshow(I)
+    plt.axis('off')
+    plt.show()
 
 # load and display QA annotations for given answer types
 """
@@ -41,17 +41,17 @@ yes/no
 number
 other
 """
-annIds = vqa.getQuesIds(ansTypes='yes/no');   
+annIds = vqa.getQuesIds(ansTypes='yes/no');
 anns = vqa.loadQA(annIds)
 randomAnn = random.choice(anns)
 vqa.showQA([randomAnn])
 imgId = randomAnn['image_id']
-imgFilename = 'COCO_' + dataSubType + '_'+ str(imgId).zfill(12) + '.jpg'
+imgFilename = 'COCO_' + dataSubType + '_' + str(imgId).zfill(12) + '.jpg'
 if os.path.isfile(imgDir + imgFilename):
-	I = io.imread(imgDir + imgFilename)
-	plt.imshow(I)
-	plt.axis('off')
-	plt.show()
+    I = io.imread(imgDir + imgFilename)
+    plt.imshow(I)
+    plt.axis('off')
+    plt.show()
 
 # load and display QA annotations for given images
 """
@@ -59,15 +59,14 @@ Usage: vqa.getImgIds(quesIds=[], quesTypes=[], ansTypes=[])
 Above method can be used to retrieve imageIds for given question Ids or given question types or given answer types.
 """
 ids = vqa.getImgIds()
-annIds = vqa.getQuesIds(imgIds=random.sample(ids,5));  
+annIds = vqa.getQuesIds(imgIds=random.sample(ids, 5));
 anns = vqa.loadQA(annIds)
 randomAnn = random.choice(anns)
-vqa.showQA([randomAnn])  
+vqa.showQA([randomAnn])
 imgId = randomAnn['image_id']
-imgFilename = 'COCO_' + dataSubType + '_'+ str(imgId).zfill(12) + '.jpg'
+imgFilename = 'COCO_' + dataSubType + '_' + str(imgId).zfill(12) + '.jpg'
 if os.path.isfile(imgDir + imgFilename):
-	I = io.imread(imgDir + imgFilename)
-	plt.imshow(I)
-	plt.axis('off')
-	plt.show()
-
+    I = io.imread(imgDir + imgFilename)
+    plt.imshow(I)
+    plt.axis('off')
+    plt.show()
--- data/download.py
+++ data/download.py
@@ -4,6 +4,7 @@ import json
 import os
 import argparse
 
+
 def download_vqa(folder):
     # Download VQA Questions (mscocoa)
     os.system('wget http://visualqa.org/data/mscoco/vqa/v2_Questions_Train_mscoco.zip -P %szip/' % (args.folder))
@@ -11,26 +12,37 @@ def download_vqa(folder):
     os.system('wget http://visualqa.org/data/mscoco/vqa/v2_Questions_Test_mscoco.zip -P %szip/' % (args.folder))
 
     # Download VQA Questions (binary abstract)
-    os.system('wget http://visualqa.org/data/abstract_v002/vqa/Questions_Binary_Train2017_abstract_v002.zip -P %szip/' % (args.folder))
-    os.system('wget http://visualqa.org/data/abstract_v002/vqa/Questions_Binary_Val2017_abstract_v002.zip -P %szip/' % (args.folder))
+    os.system(
+        'wget http://visualqa.org/data/abstract_v002/vqa/Questions_Binary_Train2017_abstract_v002.zip -P %szip/' % (
+        args.folder))
+    os.system('wget http://visualqa.org/data/abstract_v002/vqa/Questions_Binary_Val2017_abstract_v002.zip -P %szip/' % (
+    args.folder))
 
     # Download VQA Questions (abstract)
-    os.system('wget http://visualqa.org/data/abstract_v002/vqa/Questions_Train_abstract_v002.zip -P %szip/' % (args.folder))
-    os.system('wget http://visualqa.org/data/abstract_v002/vqa/Questions_Val_abstract_v002.zip -P %szip/' % (args.folder))
-    os.system('wget http://visualqa.org/data/abstract_v002/vqa/Questions_Test_abstract_v002.zip -P %szip/' % (args.folder))
-
+    os.system(
+        'wget http://visualqa.org/data/abstract_v002/vqa/Questions_Train_abstract_v002.zip -P %szip/' % (args.folder))
+    os.system(
+        'wget http://visualqa.org/data/abstract_v002/vqa/Questions_Val_abstract_v002.zip -P %szip/' % (args.folder))
+    os.system(
+        'wget http://visualqa.org/data/abstract_v002/vqa/Questions_Test_abstract_v002.zip -P %szip/' % (args.folder))
 
     # Download the VQA Annotations (mscocoa)
     os.system('wget http://visualqa.org/data/mscoco/vqa/v2_Annotations_Train_mscoco.zip -P %szip/' % (args.folder))
     os.system('wget http://visualqa.org/data/mscoco/vqa/v2_Annotations_Val_mscoco.zip -P %szip/' % (args.folder))
 
     # Download the VQA Annotations (binary abstract)
-    os.system('wget http://visualqa.org/data/abstract_v002/vqa/Annotations_Binary_Train2017_abstract_v002.zip -P %szip/' % (args.folder))
-    os.system('wget http://visualqa.org/data/abstract_v002/vqa/Annotations_Binary_Val2017_abstract_v002.zip -P %szip/' % (args.folder))
+    os.system(
+        'wget http://visualqa.org/data/abstract_v002/vqa/Annotations_Binary_Train2017_abstract_v002.zip -P %szip/' % (
+        args.folder))
+    os.system(
+        'wget http://visualqa.org/data/abstract_v002/vqa/Annotations_Binary_Val2017_abstract_v002.zip -P %szip/' % (
+        args.folder))
 
     # Download the VQA Annotations (abstract)
-    os.system('wget http://visualqa.org/data/abstract_v002/vqa/Annotations_Train_abstract_v002.zip -P %szip/' % (args.folder))
-    os.system('wget http://visualqa.org/data/abstract_v002/vqa/Annotations_Val_abstract_v002.zip -P %szip/' % (args.folder))
+    os.system(
+        'wget http://visualqa.org/data/abstract_v002/vqa/Annotations_Train_abstract_v002.zip -P %szip/' % (args.folder))
+    os.system(
+        'wget http://visualqa.org/data/abstract_v002/vqa/Annotations_Val_abstract_v002.zip -P %szip/' % (args.folder))
 
     # Download VQA Images (mscocoa)
     os.system('wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip -P %szip/' % (args.folder))
@@ -38,64 +50,73 @@ def download_vqa(folder):
     os.system('wget http://msvocds.blob.core.windows.net/coco2015/test2015.zip -P %szip/' % (args.folder))
 
     # Download VQA Images (binary abstract) (images)
-    os.system('wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_binary_train2017.zip -P %szip/' % (args.folder))
-    os.system('wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_binary_val2017.zip -P %szip/' % (args.folder))
+    os.system(
+        'wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_binary_train2017.zip -P %szip/' % (
+        args.folder))
+    os.system(
+        'wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_binary_val2017.zip -P %szip/' % (
+        args.folder))
 
     # Download VQA Images (abstract scenes)
-    os.system('wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_train2015.zip -P %szip/' % (args.folder))
-    os.system('wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_val2015.zip -P %szip/' % (args.folder))
-    os.system('wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_test2015.zip -P %szip/' % (args.folder))
+    os.system(
+        'wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_train2015.zip -P %szip/' % (
+        args.folder))
+    os.system('wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_val2015.zip -P %szip/' % (
+    args.folder))
+    os.system('wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_test2015.zip -P %szip/' % (
+    args.folder))
 
     # Unzip the files
-    
-    os.system('mkdir %sQuestions' %(folder))
-    os.system('unzip %szip/v2_Questions_Train_mscoco.zip -d %sQuestions/'%(folder, folder))
-    os.system('unzip %szip/v2_Questions_Val_mscoco.zip -d %sQuestions/'%(folder, folder))
-    os.system('unzip %szip/v2_Questions_Test_mscoco.zip -d %sQuestions/'%(folder, folder))
+
+    os.system('mkdir %sQuestions' % (folder))
+    os.system('unzip %szip/v2_Questions_Train_mscoco.zip -d %sQuestions/' % (folder, folder))
+    os.system('unzip %szip/v2_Questions_Val_mscoco.zip -d %sQuestions/' % (folder, folder))
+    os.system('unzip %szip/v2_Questions_Test_mscoco.zip -d %sQuestions/' % (folder, folder))
     os.system('unzip %szip/Questions_Binary_Train2017_abstract_v002.zip -d %sQuestions/' % (folder, folder))
     os.system('unzip %szip/Questions_Binary_Val2017_abstract_v002.zip -d %sQuestions/' % (folder, folder))
     os.system('unzip %szip/Questions_Train_abstract_v002.zip -d %sQuestions/' % (folder, folder))
     os.system('unzip %szip/Questions_Val_abstract_v002.zip -d %sQuestions/' % (folder, folder))
     os.system('unzip %szip/Questions_Test_abstract_v002.zip -d %sQuestions/' % (folder, folder))
 
-    os.system('mkdir %sAnnotations'%(folder))
-    os.system('unzip %szip/v2_Annotations_Train_mscoco.zip -d %sAnnotations/'%(folder, folder))
-    os.system('unzip %szip/v2_Annotations_Val_mscoco.zip -d %sAnnotations/' %(folder, folder))
-    os.system('unzip %szip/Annotations_Binary_Train2017_abstract_v002.zip -d %sAnnotations/'%(folder, folder))
-    os.system('unzip %szip/Annotations_Binary_Val2017_abstract_v002.zip -d %sAnnotations/' %(folder, folder))
-    os.system('unzip %szip/Annotations_Train_abstract_v002.zip -d %sAnnotations/'%(folder, folder))
-    os.system('unzip %szip/Annotations_Val_abstract_v002.zip -d %sAnnotations/' %(folder, folder))
+    os.system('mkdir %sAnnotations' % (folder))
+    os.system('unzip %szip/v2_Annotations_Train_mscoco.zip -d %sAnnotations/' % (folder, folder))
+    os.system('unzip %szip/v2_Annotations_Val_mscoco.zip -d %sAnnotations/' % (folder, folder))
+    os.system('unzip %szip/Annotations_Binary_Train2017_abstract_v002.zip -d %sAnnotations/' % (folder, folder))
+    os.system('unzip %szip/Annotations_Binary_Val2017_abstract_v002.zip -d %sAnnotations/' % (folder, folder))
+    os.system('unzip %szip/Annotations_Train_abstract_v002.zip -d %sAnnotations/' % (folder, folder))
+    os.system('unzip %szip/Annotations_Val_abstract_v002.zip -d %sAnnotations/' % (folder, folder))
 
-    
-    os.system('mkdir -p %sImages/mscocoa/test2015'%(folder))
-    os.system('mkdir %sImages/mscocoa/test2014'%(folder))
-    os.system('mkdir %sImages/mscocoa/val2014'%(folder))
-    os.system('unzip  %szip/train2014.zip -d %sImages/mscocoa/'%(folder, folder))
-    os.system('unzip %szip/val2014.zip -d %sImages/mscocoa/'%(folder, folder))
-    os.system('unzip %szip/test2015.zip -d %sImages/mscocoa/'%(folder, folder))
+    os.system('mkdir -p %sImages/mscocoa/test2015' % (folder))
+    os.system('mkdir %sImages/mscocoa/test2014' % (folder))
+    os.system('mkdir %sImages/mscocoa/val2014' % (folder))
+    os.system('unzip  %szip/train2014.zip -d %sImages/mscocoa/' % (folder, folder))
+    os.system('unzip %szip/val2014.zip -d %sImages/mscocoa/' % (folder, folder))
+    os.system('unzip %szip/test2015.zip -d %sImages/mscocoa/' % (folder, folder))
 
-    os.system('mkdir -p %s/Images/abstract_v002/train2015'%(folder))
-    os.system('mkdir %s/Images/abstract_v002/val2015'%(folder))
-    os.system('mkdir %s/Images/abstract_v002/test2015'%(folder))
-    
-    
-    os.system('unzip %szip/scene_img_abstract_v002_binary_train2017.zip -d %sImages/abstract_v002/'%(folder, folder))
-    os.system('unzip %szip/scene_img_abstract_v002_binary_val2017.zip -d %sImages/abstract_v002/' %(folder, folder))
+    os.system('mkdir -p %s/Images/abstract_v002/train2015' % (folder))
+    os.system('mkdir %s/Images/abstract_v002/val2015' % (folder))
+    os.system('mkdir %s/Images/abstract_v002/test2015' % (folder))
 
-    os.system('unzip %szip/scene_img_abstract_v002_train2015.zip -d %sImages/abstract_v002/train2015/'%(folder, folder))
-    os.system('unzip %szip/scene_img_abstract_v002_val2015.zip -d %sImages/abstract_v002/val2015/' %(folder, folder))
-    os.system('unzip %szip/scene_img_abstract_v002_test2015.zip -d %sImages/abstract_v002/test2015/'%(folder, folder))
-    
-    os.system('ln -s %sAnnotations/ .' %(folder))
-    os.system('ln -s %sQuestions/ .' %(folder))
-    os.system('ln -s %sImages/ .' %(folder))
+    os.system('unzip %szip/scene_img_abstract_v002_binary_train2017.zip -d %sImages/abstract_v002/' % (folder, folder))
+    os.system('unzip %szip/scene_img_abstract_v002_binary_val2017.zip -d %sImages/abstract_v002/' % (folder, folder))
+
+    os.system(
+        'unzip %szip/scene_img_abstract_v002_train2015.zip -d %sImages/abstract_v002/train2015/' % (folder, folder))
+    os.system('unzip %szip/scene_img_abstract_v002_val2015.zip -d %sImages/abstract_v002/val2015/' % (folder, folder))
+    os.system('unzip %szip/scene_img_abstract_v002_test2015.zip -d %sImages/abstract_v002/test2015/' % (folder, folder))
+
+    os.system('ln -s %sAnnotations/ .' % (folder))
+    os.system('ln -s %sQuestions/ .' % (folder))
+    os.system('ln -s %sImages/ .' % (folder))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Set up the dataset')
 
-    parser.add_argument('--folder', required=False, default="", help='The path to the data directory. (default : current) if you want to install in data/')
+    parser.add_argument('--folder', required=False, default="",
+                        help='The path to the data directory. (default : current) if you want to install in data/')
 
     args = parser.parse_args()
     download_vqa(args.folder)
-    print("Extracted everything. You might want to delete %szip folder" %(args.folder))
+    print(("Extracted everything. You might want to delete %szip folder" % (args.folder)))
