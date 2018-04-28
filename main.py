d1605d87a1c729c25e175e66306efe1bec6a1967
Praateek Mahajan <praateekm@gmail.com>
2018-04-28 15:50:57 -0400

    Added checkpoint saving

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from torch.autograd import Variable
 from dataloaders.VQADataset import VQADataset
 import time
 from models.combined_model import returnmodel
+import numpy as np
+import os
+from utils import utils, logger
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
@@ -40,70 +43,169 @@ torch.manual_seed(args.seed)
 if args.cuda:
     print("Cuda is available")
     torch.cuda.manual_seed(args.seed)
-
-opt = {'dir': '/home-3/pmahaja2@jhu.edu/scratch/vqa2018_data', 'images': 'Images', 'nans': 2000, 'sampleans': True,
-       'maxlength': 26, 'minwcount': 0, 'nlp': 'mcb', 'pad': 'left'}
 if args.cuda:
     kwargs = {'num_workers': int(args.num_workers), 'pin_memory': False}
 else:
     kwargs = {'num_workers': int(args.num_workers), 'pin_memory': True}
 
+
+opt = {'dir': '/home-3/pmahaja2@jhu.edu/scratch/vqa2018_data', 'images': 'Images', 'nans': 2000, 'sampleans': True,
+       'maxlength': 26, 'minwcount': 0, 'nlp': 'mcb', 'pad': 'left'}
+
+################################################
+# Create Dataset
+################################################
 train_dataset = VQADataset("train", opt)
 train_loader = train_dataset.data_loader(shuffle=True, batch_size=args.batch_size, **kwargs)
 
 test_dataset = VQADataset("val", opt)
 test_loader = test_dataset.data_loader(shuffle=False, batch_size=args.test_batch_size, **kwargs)
 
+################################################
+# Create Model and Optimizer
+################################################
+
 
 model = returnmodel(args.cuda, args.parallel)
-
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
-def train(epoch):
+################################################
+# Count model parameters
+################################################
+
+model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+params = sum([np.prod(p.size()) for p in model_parameters])
+
+################################################
+# Create log directory
+################################################
+exp_logger = None
+logdir = os.path.join(opt['dir'],args.logdir)
+
+if os.path.isdir(logdir):
+    pass
+else:
+    os.system('mkdir -p ' + os.path.join(opt['dir'],args.logdir))
+
+if exp_logger is None:
+    # Set loggers
+    exp_name = os.path.basename(logdir) # add timestamp
+    exp_logger = logger.Experiment(exp_name, logger.options)
+    exp_logger.add_meters('train', logger.make_meters())
+    exp_logger.add_meters('test', logger.make_meters())
+    exp_logger.add_meters('val', logger.make_meters())
+    exp_logger.info['model_params'] = params
+    print('Model has {} parameters'.format(exp_logger.info['model_params']))
+
+
+
+def train(epoch, logger):
     begin = time.time()
     model.train()
+    meters = logger.reset_meters('train')
+
     for batch_idx, data in enumerate(train_loader):
+        batch_size = data['question'].size(0)
+
+        # Measures the data loading time
+        meters['data_time'].update(time.time() - begin, n=batch_size)
+
         if args.cuda:
-            question, image, target = data['question'].cuda(), data['image'].float().cuda(), data['answer'].cuda()
+            question, image, target = data['question'].cuda(), data['image'].float().cuda(), data['answer'].cuda(async=True)
         else:
             question, image, target = data['question'], data['image'].float(), data['answer']
 
         question, image, target = Variable(question), Variable(image), Variable(target)
-        optimizer.zero_grad()
+
+        # Compute output and loss
         output = model(question, image)
+        torch.cuda.synchronize()
         loss = F.nll_loss(output, target)
+
+        # Log the loss
+        meters['loss'].update(loss.data[0], n=batch_size)
+
+        # Measure accuracy
+        acc1, acc5 = utils.accuracy(output.data, target.data, topk=(1, 5))
+        meters['acc1'].update(acc1[0], n=batch_size)
+        meters['acc5'].update(acc5[0], n=batch_size)
+
+        optimizer.zero_grad()
         loss.backward()
+        torch.cuda.synchronize()
+        optimizer.step()
+        torch.cuda.synchronize()
+
+        meters['batch_time'].update(time.time() - begin, n=batch_size)
+        begin = time.time()
+
         optimizer.step()
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tTime : {:.2f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.data[0], time.time() - begin))
-            begin = time.time()
-def test(loss):
-    begin = time.time()
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Acc@1 {acc1.val:.3f} ({acc1.avg:.3f})\t'
+                  'Acc@5 {acc5.val:.3f} ({acc5.avg:.3f})'.format(
+                   epoch, batch_idx, len(train_loader),
+                   batch_time=meters['batch_time'], data_time=meters['data_time'],
+                   loss=meters['loss'], acc1=meters['acc1'], acc5=meters['acc5']))
 
+    logger.log_meters('train', n=epoch)
+
+def test(logger, epoch):
     model.eval()
     test_loss = 0
-    correct = 0
-    for data in test_loader:
-       
+    meters = logger.reset_meters('val')
+    begin = time.time()
+    for batch_idx, data in enumerate(test_loader):
+        batch_size = data['target'].size(0)
+
         if args.cuda:
-            question, image, target = data['question'].cuda(), data['image'].float().cuda(), data['answer'].cuda()
+            question, image, target = data['question'].cuda(async=True), data['image'].float().cuda(async=True), data['answer'].cuda(async=True)
         else:
             question, image, target = data['question'], data['image'].float(), data['answer']
 
-        question, image, target = Variable(question), Variable(image), Variable(target)
+        question, image, target = Variable(question, volatile=True), Variable(image, volatile=True), Variable(target, volatile=True)
+
+        # Compute output and loss
         output = model(question, image)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0]  # sum up batch loss
-        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).long().cpu().sum()
+        loss = F.nll_loss(output, target).data[0]
+        test_loss += loss  # sum up batch loss
+
+        meters['loss'].update(loss.data[0], n=batch_size)
+
+        acc1, acc5 = utils.accuracy(output.data, target.data, topk=(1, 5))
+        meters['acc1'].update(acc1[0], n=batch_size)
+        meters['acc5'].update(acc5[0], n=batch_size)
+
+        meters['batch_time'].update(time.time() - begin, n=batch_size)
+
 
     test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset), time.time() - begin))
+    print('\n Test set: Average loss: {:.4f}, Acc@1 {acc1.avg:.3f} Acc@5 {acc5.avg:.3f}'
+          .format(test_loss, acc1=meters['acc1'], acc5=meters['acc5']))
+    logger.log_meters('val', n=epoch)
 
+    return meters['acc1'].avg
 
+best_acc = 0
 for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    test()
+    train(epoch, exp_logger)
+    test_acc = test(exp_logger, epoch)
+    is_best = test_acc  > best_acc
+    if is_best:
+        print("Saving model with {} accuracy".format(test_acc))
+    best_acc = max(test_acc, best_acc)
+
+    utils.save_checkpoint({
+        'epoch': epoch,
+        'best_acc1': best_acc,
+        'exp_logger': exp_logger
+    },
+        model.module.state_dict(),
+        optimizer.state_dict(),
+        logdir,
+        True,
+        True,
+        is_best)
