9e650833787c56b2bbe277d76f86e0c02736f190
Praateek Mahajan <praateekm@gmail.com>
2018-04-30 15:28:57 -0400

    experimental files which might improve performance

---
+++ dataloaders/featureset.py
@@ -0,0 +1,45 @@
+import os
+import h5py
+import torch
+import torch.utils.data as data
+
+
+class FeaturesDataset(data.Dataset):
+    def __init__(self, dataset, data_split, opt):
+        self.data_split = data_split
+        self.opt = opt
+        self.dir_extract = os.path.join(self.opt['dir'], self.opt['images'], dataset,
+                                                                   'extracted', data_split)
+        self.path_hdf5 = os.path.join(self.dir_extract, 'set.hdf5')
+        assert os.path.isfile(self.path_hdf5), \
+            'File not found in {}, you must extract the features first with extract.py'.format(self.path_hdf5)
+        self.hdf5_file = h5py.File(self.path_hdf5, 'r')  # , driver='mpio', comm=MPI.COMM_WORLD)
+        self.dataset_features = self.hdf5_file['att']
+        self.index_to_name, self.name_to_index = self._load_dicts()
+
+    def _load_dicts(self):
+        self.path_fname = os.path.join(self.dir_extract,
+                                       self.data_split + 'set.txt')
+        with open(self.path_fname, 'r') as handle:
+            self.index_to_name = handle.readlines()
+        self.index_to_name = [name[:-1] for name in self.index_to_name]  # remove char '\n'
+        self.name_to_index = {name: index for index, name in enumerate(self.index_to_name)}
+        return self.index_to_name, self.name_to_index
+
+    def __getitem__(self, index):
+        item = {}
+        item['name'] = self.index_to_name[index]
+        item['visual'] = self.get_features(index)
+        # item = torch.Tensor(self.get_features(index))
+        return item
+
+    def get_features(self, index):
+        return torch.LongTensor(self.dataset_features[index])
+
+
+    def get_by_name(self, image_name):
+        index = self.name_to_index[image_name]
+        return self[index]
+
+    def __len__(self):
+        return self.dataset_features.shape[0]
--- test_dataloader.py
+++ test_dataloader.py
@@ -1,10 +1,10 @@
 from dataloaders.VQADataset import VQADataset
 import time
 data_dir = '/home-3/pmahaja2@jhu.edu/scratch/vqa2018_data'
-opt = {'dir':data_dir,'images':'Images','nans':2000, 'sampleans':True, 'maxlength':26, 'minwcount':0, 'nlp':'mcb','pad':'left','trainsplit':'train'}
-
-begin = time.time()
+opt = {'dir':data_dir,'images':'Images','nans':2000, 'sampleans':True, 'maxlength':26, \
+       'minwcount':0, 'nlp':'mcb','pad':'left','trainsplit':'train'}
 D = VQADataset("train", opt)
+
 dl = D.data_loader(batch_size=32, num_workers=4,shuffle=False)
 print("Time to load Data loader : ",time.time() - begin)
 for i in dl:
