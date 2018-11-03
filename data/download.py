c0bd97ee8c191857ffbd64a4cbcca01710784b87
Vibhu Jawa <vibhujawa@gmail.com>
2018-11-03 14:44:21 -0400

    uncommented out code

--- data/download.py
+++ data/download.py
@@ -44,26 +44,26 @@ def download_vqa(folder):
         'wget http://visualqa.org/data/abstract_v002/vqa/Annotations_Val_abstract_v002.zip -P %szip/' % (args.folder))
 
     # # Download VQA Images (mscocoa)
-    # os.system('wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip -P %szip/' % (args.folder))
-    # os.system('wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip -P %szip/' % (args.folder))
-    # os.system('wget http://msvocds.blob.core.windows.net/coco2015/test2015.zip -P %szip/' % (args.folder))
+    os.system('wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip -P %szip/' % (args.folder))
+    os.system('wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip -P %szip/' % (args.folder))
+    os.system('wget http://msvocds.blob.core.windows.net/coco2015/test2015.zip -P %szip/' % (args.folder))
     #
     # # Download VQA Images (binary abstract) (images)
-    # os.system(
-    #     'wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_binary_train2017.zip -P %szip/' % (
-    #     args.folder))
-    # os.system(
-    #     'wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_binary_val2017.zip -P %szip/' % (
-    #     args.folder))
+    os.system(
+         'wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_binary_train2017.zip -P %szip/' % (
+         args.folder))
+    os.system(
+         'wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_binary_val2017.zip -P %szip/' % (
+         args.folder))
 
     # # Download VQA Images (abstract scenes)
-    # os.system(
-    #     'wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_train2015.zip -P %szip/' % (
-    #     args.folder))
-    # os.system('wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_val2015.zip -P %szip/' % (
-    # args.folder))
-    # os.system('wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_test2015.zip -P %szip/' % (
-    # args.folder))
+    os.system(
+         'wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_train2015.zip -P %szip/' % (
+         args.folder))
+    os.system('wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_val2015.zip -P %szip/' % (
+     args.folder))
+    os.system('wget http://visualqa.org/data/abstract_v002/scene_img/scene_img_abstract_v002_test2015.zip -P %szip/' % (
+    args.folder))
 
     # Unzip the files
 
@@ -85,24 +85,24 @@ def download_vqa(folder):
     os.system('unzip %szip/Annotations_Train_abstract_v002.zip -d %sAnnotations/' % (folder, folder))
     os.system('unzip %szip/Annotations_Val_abstract_v002.zip -d %sAnnotations/' % (folder, folder))
 
-    # os.system('mkdir -p %sImages/mscocoa/test2015' % (folder))
-    # os.system('mkdir %sImages/mscocoa/test2014' % (folder))
-    # os.system('mkdir %sImages/mscocoa/val2014' % (folder))
-    # os.system('unzip  %szip/train2014.zip -d %sImages/mscocoa/' % (folder, folder))
-    # os.system('unzip %szip/val2014.zip -d %sImages/mscocoa/' % (folder, folder))
-    # os.system('unzip %szip/test2015.zip -d %sImages/mscocoa/' % (folder, folder))
+    os.system('mkdir -p %sImages/mscocoa/test2015' % (folder))
+    os.system('mkdir %sImages/mscocoa/test2014' % (folder))
+    os.system('mkdir %sImages/mscocoa/val2014' % (folder))
+    os.system('unzip  %szip/train2014.zip -d %sImages/mscocoa/' % (folder, folder))
+    os.system('unzip %szip/val2014.zip -d %sImages/mscocoa/' % (folder, folder))
+    os.system('unzip %szip/test2015.zip -d %sImages/mscocoa/' % (folder, folder))
     #
-    # os.system('mkdir -p %s/Images/abstract_v002/train2015' % (folder))
-    # os.system('mkdir %s/Images/abstract_v002/val2015' % (folder))
-    # os.system('mkdir %s/Images/abstract_v002/test2015' % (folder))
+    os.system('mkdir -p %s/Images/abstract_v002/train2015' % (folder))
+    os.system('mkdir %s/Images/abstract_v002/val2015' % (folder))
+    os.system('mkdir %s/Images/abstract_v002/test2015' % (folder))
     #
-    # os.system('unzip %szip/scene_img_abstract_v002_binary_train2017.zip -d %sImages/abstract_v002/' % (folder, folder))
-    # os.system('unzip %szip/scene_img_abstract_v002_binary_val2017.zip -d %sImages/abstract_v002/' % (folder, folder))
+    os.system('unzip %szip/scene_img_abstract_v002_binary_train2017.zip -d %sImages/abstract_v002/' % (folder, folder))
+    os.system('unzip %szip/scene_img_abstract_v002_binary_val2017.zip -d %sImages/abstract_v002/' % (folder, folder))
     #
-    # os.system(
-    #     'unzip %szip/scene_img_abstract_v002_train2015.zip -d %sImages/abstract_v002/train2015/' % (folder, folder))
-    # os.system('unzip %szip/scene_img_abstract_v002_val2015.zip -d %sImages/abstract_v002/val2015/' % (folder, folder))
-    # os.system('unzip %szip/scene_img_abstract_v002_test2015.zip -d %sImages/abstract_v002/test2015/' % (folder, folder))
+    os.system(
+         'unzip %szip/scene_img_abstract_v002_train2015.zip -d %sImages/abstract_v002/train2015/' % (folder, folder))
+    os.system('unzip %szip/scene_img_abstract_v002_val2015.zip -d %sImages/abstract_v002/val2015/' % (folder, folder))
+    os.system('unzip %szip/scene_img_abstract_v002_test2015.zip -d %sImages/abstract_v002/test2015/' % (folder, folder))
 
     os.system('ln -s %sAnnotations/ .' % (folder))
     os.system('ln -s %sQuestions/ .' % (folder))
